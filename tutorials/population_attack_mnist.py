1f128a7e87712b4af0f3d87a0c5217e4d3bb1bef
sarthakpati <sarthak.pati@hotmail.com>
2022-05-13 11:16:24 -0400

    updated logging, all nm and gm options trained on 20220428_2024, updated populatoin, train, test paths

--- tutorials/population_attack_mnist.py
+++ tutorials/population_attack_mnist.py
@@ -34,7 +34,7 @@ logger = logging.getLogger()
 num_data_in_class = 1000
 
 device = 'cuda'
-exp_name = 'tutorial_pytorch_mnist'
+exp_name = 'tutorial_pytorch_sbu'
 
 # GaNDLF config path here
 gandlf_config_path = '/home/aspaul/GaNDLF/samples/config_classification_MNIST.yaml'
@@ -42,15 +42,16 @@ gandlf_config_path = '/home/aspaul/GaNDLF/samples/config_classification_MNIST.ya
 gandlf_config = parseConfig(gandlf_config_path)
 gandlf_config['device'] = gandlf_config.get('device', device)
 
-population_csv_path = "/home/aspaul/MNIST_dataset_png/mnist_png/MNIST_pm_population_class_balanced.csv"
-train_csv_path = "/home/aspaul/MNIST_dataset_png/mnist_png/MNIST_pm_train_class_balanced.csv"
-test_csv_path =  "/home/aspaul/MNIST_dataset_png/mnist_png/MNIST_pm_test_class_balanced.csv"
+population_csv_path = "/cbica/home/patis/comp_space/testing/ml_privacy_meter/SBU_pm_population_class_balanced.csv"
+train_csv_path = "/cbica/home/patis/comp_space/testing/ml_privacy_meter/SBU_pm_train_class_balanced.csv"
+test_csv_path =  "/cbica/home/patis/comp_space/testing/ml_privacy_meter/SBU_pm_test_class_balanced.csv"
 
 batch_size = 1
 # We will keep this batch size as some code expects it
 assert batch_size == 1
 
-model_filepath = '/home/aspaul/GaNDLF/experiment_e15_imagenetvgg16_modeleveryepoch/model_dir/imagenet_vgg16_best.pth.tar'
+base_model_path = "/cbica/home/patis/comp_space/testing/gandlf_dp_experiments_20220428_2024"
+# model_filepath = '/home/aspaul/GaNDLF/experiment_e15_imagenetvgg16_modeleveryepoch/model_dir/imagenet_vgg16_best.pth.tar'
 
 # defining dict for models - key is the string and the value is the transform object
 global_models_dict = {
@@ -167,84 +168,103 @@ def get_model_class_and_loaders(parameters, train_csv_path, test_csv_path, popul
 
 if __name__ == '__main__':
 
-    fhandler = logging.FileHandler(filename='population_attack_' + exp_name + '.log', mode='a')
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fhandler.setFormatter(formatter)
-    logger.addHandler(fhandler)
-    logger.setLevel(logging.CRITICAL)
-
     # get dataset (loaders) (preprocess script is what changes here for new dataset)
     target_model_class, train_loader, test_loader, population_loader = get_model_class_and_loaders(parameters=gandlf_config, 
                                                                                                    population_csv_path=population_csv_path, 
                                                                                                    train_csv_path=train_csv_path, 
                                                                                                    test_csv_path=test_csv_path)
 
-    logger.critical(f"Running population attack against model {target_model_class}\n")
-    logger.critical(f"Model parms from file at {model_filepath}\n")
-    logger.critical(f"Using GANDLF config at: {gandlf_config_path}\n")
-    logger.critical(f"PM Population data from: {population_csv_path}\n")
-    logger.critical(f"PM Train samples from: {train_csv_path}\n")
-    logger.critical(f"PM test samples from: {test_csv_path}\n")
+    # configs that SP has trained for exp ID 20220428_2024
+    noise_multipliers = [0.0, 0.25, 0.5, 0.75, 1.0, 2.0, 5.0, 10]
+    max_grad_norms = [1.0, 2.0, 5.0, 10]
+    
+    ## ".2f" is needed because that's how strings are getting saved
+    for noise_multiplier in noise_multipliers:
+        nm_str = format(noise_multiplier, ".2f")
+        nm_dir = os.path.join(base_model_path, "nm_" + nm_str)
+        for grad in max_grad_norms:
+            gm_str = format(grad, ".2f")
+            nm_gm_dir = os.path.join(nm_dir, "gm_" + gm_str)
+            model_filepath = os.path.join(nm_gm_dir, "imagenet_vgg16_best.pth.tar")
 
-    if os.path.isfile(model_filepath):
-        logger.critical(f"Model already trained. Continuing...")
-    else:
-        raise RuntimeError('This script was not intended to be used if the model is not trained yet.')
+            ## set up exp-specific logging
 
-    x_train = LoaderRestrictor(base_loader=train_loader, 
-                            restrictions= ('feature', None), 
-                            dict_to_feature=partial(gandlf_dict_to_feature, gandlf_config=gandlf_config), 
-                            dict_to_label=None)
-    y_train = LoaderRestrictor(base_loader=train_loader, 
-                            restrictions= ('label', None), 
-                            dict_to_feature=None, 
-                            dict_to_label=partial(gandlf_dict_to_label, gandlf_config=gandlf_config))
+            current_exp_name = "sbu_nm_" + nm_str + "_gm_" + gm_str
+            
+            fhandler = logging.FileHandler(filename='population_attack_' + current_exp_name + '.log', mode='a')
+            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            fhandler.setFormatter(formatter)
+            logger.addHandler(fhandler)
+            logger.setLevel(logging.CRITICAL)
+            logger.critical(f"Running population attack against model {target_model_class}\n")
+            logger.critical(f"Using GANDLF config at: {gandlf_config_path}\n")
+            logger.critical(f"PM Population data from: {population_csv_path}\n")
+            logger.critical(f"PM Train samples from: {train_csv_path}\n")
+            logger.critical(f"PM test samples from: {test_csv_path}\n")
 
-    x_test = LoaderRestrictor(base_loader=test_loader, 
-                            restrictions= ('feature', None), 
-                            dict_to_feature=partial(gandlf_dict_to_feature, gandlf_config=gandlf_config), 
-                            dict_to_label=None)
-    y_test = LoaderRestrictor(base_loader=test_loader, 
-                            restrictions= ('label', None), 
-                            dict_to_feature=None, 
-                            dict_to_label=partial(gandlf_dict_to_label, gandlf_config=gandlf_config))
+            
+            logger.critical(f"Model parms from file at {model_filepath}\n")
+            
+            # original code
+            if os.path.isfile(model_filepath):
+                logger.critical(f"Model already trained. Continuing...")
+            else:
+                raise RuntimeError('This script was not intended to be used if the model is not trained yet.')
 
-    x_population = LoaderRestrictor(base_loader=population_loader, 
+            x_train = LoaderRestrictor(base_loader=train_loader, 
                                     restrictions= ('feature', None), 
                                     dict_to_feature=partial(gandlf_dict_to_feature, gandlf_config=gandlf_config), 
                                     dict_to_label=None)
-    y_population = LoaderRestrictor(base_loader=population_loader, 
+            y_train = LoaderRestrictor(base_loader=train_loader, 
                                     restrictions= ('label', None), 
                                     dict_to_feature=None, 
                                     dict_to_label=partial(gandlf_dict_to_label, gandlf_config=gandlf_config))
-    
 
-    # create population attack object
-    loss_fn = tf.keras.losses.CategoricalCrossentropy(from_logits=True, reduction=tf.keras.losses.Reduction.NONE)
-    # loss_fn = CrossEntropyLoss(reduction='none')
-    population_attack_obj = ml_privacy_meter.attack.population_meminf.PopulationAttack(
-        exp_name=exp_name,
-        gandlf_config=gandlf_config,
-        x_population=x_population, y_population=y_population,
-        x_target_train=x_train, y_target_train=y_train,
-        x_target_test=x_test, y_target_test=y_test,
-        target_model_filepath=model_filepath,
-        target_model_type=ml_privacy_meter.utils.attack_utils.MODEL_TYPE_PYTORCH,
-        target_model_class=target_model_class,  # pass in the model class for pytorch
-        loss_fn=loss_fn,
-        num_data_in_class=num_data_in_class, 
-        num_classes=len(gandlf_config['model']['class_list']), 
-        seed=1234, 
-        device=device, 
-        logger=logger
-    )
+            x_test = LoaderRestrictor(base_loader=test_loader, 
+                                    restrictions= ('feature', None), 
+                                    dict_to_feature=partial(gandlf_dict_to_feature, gandlf_config=gandlf_config), 
+                                    dict_to_label=None)
+            y_test = LoaderRestrictor(base_loader=test_loader, 
+                                    restrictions= ('label', None), 
+                                    dict_to_feature=None, 
+                                    dict_to_label=partial(gandlf_dict_to_label, gandlf_config=gandlf_config))
 
-    population_attack_obj.prepare_attack()
+            x_population = LoaderRestrictor(base_loader=population_loader, 
+                                            restrictions= ('feature', None), 
+                                            dict_to_feature=partial(gandlf_dict_to_feature, gandlf_config=gandlf_config), 
+                                            dict_to_label=None)
+            y_population = LoaderRestrictor(base_loader=population_loader, 
+                                            restrictions= ('label', None), 
+                                            dict_to_feature=None, 
+                                            dict_to_label=partial(gandlf_dict_to_label, gandlf_config=gandlf_config))
+            
 
-    alphas = [0.1, 0.3, 0.5]
+            # create population attack object
+            loss_fn = tf.keras.losses.CategoricalCrossentropy(from_logits=True, reduction=tf.keras.losses.Reduction.NONE)
+            # loss_fn = CrossEntropyLoss(reduction='none')
+            population_attack_obj = ml_privacy_meter.attack.population_meminf.PopulationAttack(
+                exp_name=current_exp_name,
+                gandlf_config=gandlf_config,
+                x_population=x_population, y_population=y_population,
+                x_target_train=x_train, y_target_train=y_train,
+                x_target_test=x_test, y_target_test=y_test,
+                target_model_filepath=model_filepath,
+                target_model_type=ml_privacy_meter.utils.attack_utils.MODEL_TYPE_PYTORCH,
+                target_model_class=target_model_class,  # pass in the model class for pytorch
+                loss_fn=loss_fn,
+                num_data_in_class=num_data_in_class, 
+                num_classes=len(gandlf_config['model']['class_list']), 
+                seed=1234, 
+                device=device, 
+                logger=logger
+            )
 
-    logger.critical(f"Running with alphas: {alphas}")
+            population_attack_obj.prepare_attack()
 
-    population_attack_obj.run_attack(alphas=alphas)
+            alphas = [0.1, 0.3, 0.5]
 
-    population_attack_obj.visualize_attack(alphas=alphas)
+            logger.critical(f"Running with alphas: {alphas}")
+
+            population_attack_obj.run_attack(alphas=alphas)
+
+            population_attack_obj.visualize_attack(alphas=alphas)
